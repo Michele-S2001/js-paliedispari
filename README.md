4b21d6676c974446dc47c9b2b83d8afb0dd49edc
MicheleSerafini <serafini.michele01@gmail.com>
2023-10-12 16:19:48 +0200

    Feat: palindrome script

---
+++ README.md
@@ -0,0 +1,18 @@
+# Palindroma
+
+- Chiedere all’utente di inserire una parola
+  - dichiarare una variabile che chieda di inserire una parola tramite prompt, e far in modo che i caratteri inseriti siano tutti minuscoli
+
+- Creare una funzione per capire se la parola inserita è palindroma
+  - dichiarare una funzione che abbia come argomento la variabile contentente la parola inserita dall'utente
+  - all'interno:
+    - creo un array1 vuoto
+    - creare un ciclo for che mi scomponga ogni lettera della parola inserita dall'utente e che inserisca lettera per lettera nell'array1
+
+    - creo un array2 vuoto
+    - creare un ciclo for invertito che mi scomponga ogni lettera della parola inserita dall'utente e che inserisca lettera per lettera nell'array2
+
+    - SE  i due array in formato stringa sono uguali:
+      - return true
+    - ALTRIMENTI 
+      - return false
--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
-console.log('funziono');
+// ->->-> palindroma <-<-<-
+
+// chiedere una parola all'utente
+const userWord = (prompt('Inserisci una parola')).toLocaleLowerCase();
+
+if (isPalindrome(userWord)) {
+  console.log(`"${userWord}" è una parola palindroma`);
+} else {
+  console.log(`"${userWord}" non è una parola palindroma`);
+}
+
+function isPalindrome (word) {
+ 
+  const wordFromLeftToRight = [];
+  for (let i = 0; i < userWord.length; i++) {
+    const charToRight = userWord[i];
+    wordFromLeftToRight.push(charToRight);
+  }
+  console.log(wordFromLeftToRight);
+  
+  const wordFromRightToLeft = [];
+  for (let i = userWord.length - 1; i >= 0; i--) {
+    const charToLeft = userWord[i];
+    wordFromRightToLeft.push(charToLeft);
+  }
+  console.log(wordFromRightToLeft);
+
+  if (wordFromLeftToRight.toString() === wordFromRightToLeft.toString()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
